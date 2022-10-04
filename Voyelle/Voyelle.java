4e89089bb298714d9086d902663b2714b951c0f5
RegameyManuel <regameymanuel@gmail.com>
2022-10-04 21:53:27 +0200

    ajout Voyelle

---
+++ Voyelle/Voyelle.java
@@ -0,0 +1,48 @@
+package Voyelle;
+
+import java.util.Scanner;
+
+public class Voyelle {
+
+    public static void main(String[] args) {
+        int i, j = 0;
+        String str, aei = "aeiouy";
+
+        try {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Bonjour veuillez entrer votre texte :");
+            str = sc.next();
+
+            /*******************************************************/
+            /* j = (str.replaceAll("(?i)[^aeiouy]", "")).length(); */
+            /*******************************************************/
+
+            /*
+             * for (i = 0; i < str.length(); i++) {
+             * if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' ||
+             * str.charAt(i) == 'o'
+             * || str.charAt(i) == 'u') {
+             * j++;
+             * }
+             * }
+             */
+
+            for (i = 0; i < str.length(); i++) {
+                if (aei.indexOf(str.substring(i, i + 1)) != -1) {
+                    j++;
+                }
+
+            }
+
+            sc.close();
+            System.out.println("Le texte comporte " + j + " voyelles!");
+
+        } catch (NumberFormatException e) {
+            System.out.println("Format incorrect");
+            System.exit(0);
+        }
+
+    }
+
+}
